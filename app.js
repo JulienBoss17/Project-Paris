3f38361abd85dc8bea9bb0803cdc7c2562893166
t <you@example.com>
2025-03-14 17:55:18 +0100

    "port"

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ mongoose.connect(process.env.MONGO_URI, {
 .catch(err => console.error("❌ Erreur de connexion MongoDB :", err));
 
 
-const port= process.env.PORT || 3000;
+const port= process.env.PORT || 80;
 
 app.listen(port,() => {
     console.log("listening http://localhost:"+ port)
