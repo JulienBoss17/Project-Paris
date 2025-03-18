ccaa2551069d83293258326294b389ca32bf22f4
t <you@example.com>
2025-03-18 17:10:30 +0100

    "middlewares"

--- Middlewares/verifysession2.js
+++ Middlewares/verifysession2.js
@@ -2,12 +2,16 @@
 
 function isAuthenticated() {
     return async (req, res, next) => {
+    if (req.session.userId) {
         if (req.session && req.session.userId) {  
             return next();  
         } else {
             res.redirect("/compte");
         }
+    } else {
+        res.redirect("/")
     }
+}
 }
 
 module.exports = isAuthenticated;
